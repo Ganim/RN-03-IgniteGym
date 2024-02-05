01917a52cd43d895b6372df5debb55024873f6dc
Guilherme <guilhermeganim@hotmail.com>
2024-02-05 14:44:32 -0300

    Add: Componente Library Study Module

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
-import { StatusBar, Text, View } from 'react-native';
+import { LogBox, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { NativeBaseProvider } from 'native-base';
+import { Loading } from '@components/Loading';
+import { THEME } from 'src/theme';
+import { SignIn } from '@screens/SignIn';
+import { useEffect } from 'react';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
+  useEffect(() => {
+    LogBox.ignoreLogs(['In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app.']);
+    }, []);
+    
   return (
-    <View >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Text>Open up App.tsx to start working on your app!</Text> : <View/>}
-    </View>
+      {fontsLoaded ? <SignIn/> : <Loading/>}
+    </NativeBaseProvider>
   );
 }
 
